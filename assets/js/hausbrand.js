cd9892d9c52e0806f1deaed267b21ef6dcc97143
Brandon Mandzik <brandon@spot9.com>
2021-04-06 22:41:43 +0200

    finishing

--- assets/js/hausbrand.js
+++ assets/js/hausbrand.js
@@ -1,8 +1,16 @@
 // https://manage.auth0.com/dashboard/eu/hausbraand/applications/aM2OX9ZBJSzRupNYeWWtPXZauw3OdLIG/quickstart
 
+/**
+ * CONFIGURATION
+*/
+
+// init auth0
 let auth0 = null;
+
+// fetch configs
 const fetchAuthConfig = () => fetch("/auth_config.json")
 
+// configure auth0 client for furhter usage
 const configureClient = async () => {
     const response = await fetchAuthConfig()
     const config = await response.json()
@@ -13,58 +21,105 @@ const configureClient = async () => {
     })
 }
 
+const requireAuth = async (targetUrl) => {
+    const isAuthenticated = await auth0.isAuthenticated();
+
+    if (isAuthenticated) {
+        return;
+    }
+
+    return login(targetUrl);
+};
+
+// updateUI will handle the visibiliy based on user
+const updateUI = async () => {
+    try {
+        const isAuthenticated = await auth0.isAuthenticated();
+
+        if (isAuthenticated) {
+            var gatedElements = document.getElementsByClassName("gated-content")
+            for (var i = gatedElements.length - 1; i >= 0; i--) {
+                gatedElements[i].classList.remove("gated-content");
+            }
+        } else {
+            var gatedElements = document.getElementsByClassName("gated-content")
+            for (var i = gatedElements.length - 1; i >= 0; i--) {
+                gatedElements[i].parentNode.removeChild(gatedElements[i]);
+                // gatedElements[i].classList.add("gated-content");
+            }
+        }
+        document.getElementById("btn-logout").disabled = !isAuthenticated
+        document.getElementById("btn-login").disabled = isAuthenticated
+    } catch (err) {
+        console.log("UI update Error!", err)
+    }
+}
+
+/**
+ * ---MAIN---
+ * @returns 
+ */
+
+// Checks User state .Onload
 window.onload = async () => {
     await configureClient()
 
-    updateUI()
+    if (/\/work\/.+/.test(window.location.pathname)){
+        requireAuth("/work")
+    }
 
     const isAuthenticated = await auth0.isAuthenticated()
 
     if (isAuthenticated) {
-        // show the gated content
-        return
+        console.log("> User is authenticated");
+        updateUI();
+        return;
     }
 
+    console.log("> User not authenticated");
+    // parsing redirect
     const query = window.location.search
     if (query.includes("code=") && query.includes("state=")) {
-
-        await auth0.handleRedirectCallback()
-
-        updateUI()
-
+        try {
+            await auth0.handleRedirectCallback()
+        } catch (err) {
+            console.log("error parsing redirect:", err)
+        }
         window.history.replaceState({}, document.title, "/")
     }
+    updateUI()
 }
 
-const updateUI = async () => {
-    const isAuthenticated = await auth0.isAuthenticated();
-    console.log(isAuthenticated)
-    document.getElementById("btn-logout").disabled = !isAuthenticated
-    document.getElementById("btn-login").disabled = isAuthenticated
+/**
+ * LOGIN & SIGN-UP METHODS  
+ */
 
-    if (isAuthenticated) {
-        var gatedElements = document.getElementsByClassName("gated-content")
-        for(var i = gatedElements.length - 1; i >= 0; i--){
-            gatedElements[i].classList.remove("gated-content")
+const login = async (targetUrl) => {
+    try {
+        console.log("Logging in")
+        const options = {
+            redirect_uri: window.location.origin
+        };
+
+        if (targetUrl) {
+            options.appState = { targetUrl };
         }
-    } else {
-        var gatedElements = document.getElementsByClassName("gated-content")
-        for(var i = gatedElements.length - 1; i >= 0; i--){
-            gatedElements[i].remove();
-        }
+
+        await auth0.loginWithRedirect(options);
+    } catch (err) {
+        console.log("Login failed", err)
     }
-}
-
-const login = async () => {
-    await auth0.loginWithRedirect({
-        redirect_uri: window.location.origin
-    })
 }
 
 const logout = () => {
-    auth0.logout({
-        returnTo: window.location.origin
-    });
+    try {
+        console.log("Logging out");
+        auth0.logout({
+            returnTo: window.location.origin
+        })
+    } catch (err) {
+        console.log("Log out failed", err);
+    }
 };
 
 
