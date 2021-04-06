b13d84f557fc1d3d6a7ba46677d17d0474f7c3de
Brandon Mandzik <brandon@spot9.com>
2021-04-06 12:59:36 +0200

    VIP section wip

--- assets/js/hausbrand.js
+++ assets/js/hausbrand.js
@@ -7,14 +7,29 @@ const configureClient = async () => {
     const response = await fetchAuthConfig()
     const config = await response.json()
 
-    auth0 = await createAuth0Client ({
+    auth0 = await createAuth0Client({
         domain: config.domain,
-        client_id = config. clientID
+        client_id: config.clientID
     })
 }
 
-window.oload = async () => {
+window.onload = async () => {
     await configureClient()
+
+    updateUI()
+}
+
+const updateUI = async () => {
+    const isAuthenticated = await auth0.isAuthenticated();
+
+    document.getElementById("btn-logout").disabled = !isAuthenticated;
+    document.getElementById("btn-login").disabled = isAuthenticated;
+}
+
+const login = async () => {
+    await auth0.loginWithRedirect({
+        redirect_uri: window.location.origin
+    })
 }
 
 
